f35f7b0f40569f5af8aaad04931152ac74bc7b1a
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-04 15:59:16 +0200

    inserito prompts e funzione x generare random number

--- main.js
+++ main.js
@@ -41,3 +41,13 @@ palindroma(word);
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
+let userChoice = prompt("scegli un numero pari o dispari");
+let userNumber = Number( prompt("inseire un numero  compreso tra 1 e 5"));
+
+// let randomNumber = Math.floor((Math.random()*5) +1);
+
+function randomNumber (){
+    return Math.floor((Math.random()*5) +1)
+}
+
+randomNumber()
