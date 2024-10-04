3a9fb9f95a7547f333940a29412cbe2caaa0e413
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-04 14:53:59 +0200

    suddiviso la parola in caratteri

--- main.js
+++ main.js
@@ -3,4 +3,10 @@
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
 
-let word = prompt("inserisci una parola");/* creo il prompt per far inserire la parola allo useer */
+let word = prompt("inserisci una parola");/* creo il prompt per far inserire la parola allo useer    */
+
+const ch = word.split('');/* suddivido la parola in lettere */
+console.log(ch);
+
+
+
