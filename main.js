95932d177e2894a82e9a8ab37f2980ddcb1fa056
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-04 15:11:29 +0200

    invertito ordine ch e riunito i ch

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Palidroma
-// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindromaPari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
 
 let word = prompt("inserisci una parola");/* creo il prompt per far inserire la parola allo useer    */
@@ -9,4 +8,21 @@ const ch = word.split('');/* suddivido la parola in lettere */
 console.log(ch);
 
 
+const reverseCh = ch.reverse();/* inverto lordine dei caratteri */
+console.log(reverseCh);
 
+
+const reversedWord = reverseCh.join('');/* riunisco i caratteri in una stringa */
+console.log(reversedWord);
+
+
+
+
+
+
+
+
+
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
